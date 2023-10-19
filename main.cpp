7e04cf7b4a8510a5adc56136949c4e5a9687655d
DaneWinterboer <148388845+DaneWinterboer@users.noreply.github.com>
2023-10-18 20:42:16 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,105 +1,100 @@
-/*
-Author: Dane Winterboer
-Team Members: Aniruddha Biswas Atanu, Mohamed Amine Saoud
-Course: CS181: Foundations of Computer Science II
-Instructor: Dr. Shahadat
-Date: Oct. 20th, 2023
-Purpose: Program fullfills the requirements for team assignment 04.
-Description: Program utlizes a dynamically allocated arrary, which contains
-pointers to instances of a derived class "Manager," to determine the highest
-and average salary of a user defined number of manager level employees.
-*/
+// Author: Dane Winterboer
+// Team Members: Aniruddha Biswas Atanu, Mohamed Amine Saoud
+// Course: CS181: Foundations of Computer Science II
+// Instructor: Dr. Shahadat
+// Date: Sept. 28th, 2023
+// Purpose: Program fullfills the requirements for team assignment 03.
+// Description: Program utlizes two classes to analyze the cost of ownership
+// of two different computers over the same number of years.  
 
 #include <iostream>
 #include <iomanip>
-#include "Manager.h"
+#include "Laptop.h"
+#include "Desktop.h"
 
 using namespace std;
 
 int main()
 {
   // Program constants
-  const unsigned DECIMAL_PLACES = 2;
-  
-  // Gets number of managers from user
-  size_t totalManagers;
-  cout << "Enter number of managers: ";
-  cin >> totalManagers;
-  cout << endl; // blank line
-  
-  // Dynamically allocates arrary of managers
-  Manager* managers = new Manager[totalManagers];
-  
-  // Gets user input of manager information
-  for(size_t index = 0; index < totalManagers; index++)
-  {
-    // Gets manager name
-    cout << "Enter manager " << index << " name: ";
-    string name;
-    string dummy;
-    getline(cin, dummy);
-    getline(cin, name);
-    
-    // Gets manager name
-    cout << "Enter manager " << index << " hourly wage: ";
-    double wage;
-    cin >> wage;
-    
-    // Gets manager hours
-    cout << "Enter manager " << index << " hours worked: ";
-    double hours;
-    cin >> hours;
-    
-    // Gets manager bonus
-    cout << "Enter manager " << index << " bonus: ";
-    double bonus;
-    cin >> bonus;
-    cout << endl; // blank line
+  const unsigned DECIMIAL_PLACES = 2;
 
-    
-    // Creates a new object initlized with user input
-    Manager* temp = new Manager(bonus, name, wage, hours);
-    
-    // Stores new object in manager array
-    managers[index] = *temp;
-    
-    // Removes temp from active memory
-    temp = nullptr;
-    delete temp;
-  }
-  
-  // Finds highest paid manager in list
-  size_t highestPayIndex = 0; // tracking varaiable for highest paied index
-  
-  for(size_t index = 1; index < totalManagers; index++)
-  {
-    // If true, assigns newest highest pay manager and its index position
-    if(managers[highestPayIndex].calcPay() < managers[index].calcPay())
-    {
-      highestPayIndex = index;
-    }
-  }
-  
-  // Displays highest paid manager
-  cout << "Highest paid manager is " <<
-          managers[highestPayIndex].getName() << fixed << 
-          setprecision(DECIMAL_PLACES) << " who is paid $" <<
-          managers[highestPayIndex].calcPay() << endl;
-  
-  // Tracking variables for average calculation
-  double totalPay = 0;
+  // Gets laptop info from user
+  cout << "Enter name of laptop computer: ";
+  string laptop_name;
+  getline(cin, laptop_name);
 
-  // Sums all managers pay
-  for(size_t index = 0; index < totalManagers; index++)
+  cout << "Enter cost of laptop computer: ";
+  double laptop_cost;
+  cin >> laptop_cost;
+
+  // Gets desktop info from user
+  cout << "Enter name of desktop computer: ";
+  string desktop_name;
+  string dummy;
+  getline(cin, dummy);
+  getline(cin, desktop_name);
+
+  cout << "Enter cost of desktop computer: ";
+  double desktop_cost;
+  cin >> desktop_cost;
+
+  // Gets general info from user
+  cout << "How many hours will each computer be used in a year? ";
+  double hours;
+  cin >> hours;
+
+  cout << "How many years will you own each computer? ";
+  unsigned years;
+  cin >> years;
+  cout << endl;
+
+  // Initlizes laptop object
+  Laptop laptop1(laptop_name, laptop_cost, hours, years);
+
+  // Initlizes desktop object
+  Desktop desktop1(desktop_name, desktop_cost, hours, years);
+
+  // Calculators total cost over time for both objects
+  laptop1.costOverTime();
+  desktop1.costOverTime();
+  
+
+  // Formating cout
+  cout << fixed << setprecision(0);
+
+  // Determines which computer cost more and prints difference
+  if (laptop1 > desktop1)
   {
-    totalPay += managers[index].calcPay();
+    // If laptop is more expensive than the desktop
+    cout << "The cost of " << laptop1.getComputerName() << " is $" << 
+            abs(laptop1 - desktop1) << " higher than the " << 
+            desktop1.getComputerName() << "." << endl;
   }
-   
-  // Calculates average
-  double averagePay = totalPay / static_cast<double>(totalManagers);
-  
-  // Displays average
-  cout << "Average pay is $" << averagePay << endl;
-  
+  else if (laptop1 < desktop1)
+  {
+    cout << "The cost of " << desktop1.getComputerName() << " is $" << 
+            abs(laptop1 - desktop1) << " higher than the " << 
+            laptop1.getComputerName() << endl;
+  }
+  else
+  {
+    cout << "The cost of " << desktop1.getComputerName() <<
+            " is the same as " << laptop1.getComputerName() << "." << endl; 
+  }
+
+  // Formating cout
+  cout << fixed << setprecision(DECIMIAL_PLACES);
+
+  // Prints info about laptop
+  cout << "Cost of owning a " << laptop1.getComputerName() << " over " <<
+          laptop1.getYearsUsed() << " years is $" << 
+          laptop1.getTotalCostOverTime() << endl;
+
+  // Prints info about desktop
+  cout << "Cost of owning a " << desktop1.getComputerName() << " over " <<
+          desktop1.getYearsUsed() << " years is $" << 
+          desktop1.getTotalCostOverTime() << endl;
+
   return 0;
 }
